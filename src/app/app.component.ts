4629be1b3a9e37f11a1ce64eabf0021f55747d06
Ogied <diegorg.dev@gmail.com>
2021-05-01 15:13:39 -0500

    End of module 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Product } from './product.model';
 export class AppComponent {
   title = 'platzi-store';
   items = ['jett', 'phoenix', 'reyna', 'sage'];
+  power = 10;
   products: Product[] = [
     {
       id: '1',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './component/product.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CartComponent } from './cart/cart.component';
+import { ExponentialPipe } from './exponential.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    CartComponent
+    CartComponent,
+    ExponentialPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
